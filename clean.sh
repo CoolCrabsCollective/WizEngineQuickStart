5dd8b2a2d6838dbf73b5ba9aa1d82317312ba563
Alexander Winter <a.w1nter@hotmail.com>
2022-02-13 14:40:59 -0500

    Still stuck

---
+++ clean.sh
@@ -0,0 +1,17 @@
+rm -rf client/build
+rm -rf client/build-switch
+rm -rf client/cmake-build-debug
+rm -rf client/cmake-build-release
+rm -rf client/out
+
+rm -rf common/build
+rm -rf common/build-switch
+rm -rf common/cmake-build-debug
+rm -rf common/cmake-build-release
+rm -rf common/out
+
+rm -rf server/build
+rm -rf server/build-switch
+rm -rf server/cmake-build-debug
+rm -rf server/cmake-build-release
+rm -rf server/out
