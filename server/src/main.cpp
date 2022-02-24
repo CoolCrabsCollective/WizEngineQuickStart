96e98bfb522668c44f0a5c7f5786215b6d77679e
Cedric Martens <cedricmartens98@gmail.com>
2022-02-24 12:19:14 -0500

    Clients connecting

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,13 +5,17 @@
 #include <WIZ/logging/DailyFileLogger.h>
 #include <WIZ/logging/StdOutLogger.h>
 
+#include <vector>
+
+std::vector<sf::TcpSocket*> clients;
 int main()    
 {
-    std::unique_ptr<Logger> logger = std::unique_ptr<Logger>(new TagLoggerWrapper(
-            new DateTimeLoggerWrapper(
-                    new MultiLogger({
-                            new DailyFileLogger("logs/", LogLevel::DEBUG),
-                            new StdOutLogger(LogLevel::DEBUG)
+    bool isRunning = true;
+    std::unique_ptr<wiz::Logger> logger = std::unique_ptr<wiz::Logger>(new wiz::TagLoggerWrapper(
+            new wiz::DateTimeLoggerWrapper(
+                    new wiz::MultiLogger({
+                            new wiz::DailyFileLogger("logs/", wiz::LogLevel::DEBUG),
+                            new wiz::StdOutLogger(wiz::LogLevel::DEBUG)
                     }), "[%H:%M:%S]")));
 
     logger->info("Starting test server");
@@ -25,13 +29,27 @@ int main()
     }
 
     // accept a new connection
-    sf::TcpSocket client;
-    if (listener.accept(client) != sf::Socket::Done)
+    while(isRunning)
     {
-        logger->error("Failed to accept connection");
+        sf::TcpSocket* client = new sf::TcpSocket();
+        if (listener.accept(*client) != sf::Socket::Done)
+        {
+            logger->error("Failed to accept connection");
+            delete client;
+        }
+        else
+        {
+            logger->info("Client connected");
+            clients.push_back(client);
+            std::string msgGreetings("What's up gamer!");
+            client->send(msgGreetings.c_str(), msgGreetings.size() + 1);
+        }
     }
 
-    logger->info("Client connected");
+    for(uint32_t i = 0; i < clients.size(); i++)
+    {
+        delete clients[i];
+    }
 
     return 0;    
 }    
