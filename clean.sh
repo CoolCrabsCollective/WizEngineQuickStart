409d5d7751893f9a9cfdb9025ed8d2d249dfb900
Alexander Winter <a.w1nter@hotmail.com>
2022-03-11 22:23:19 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -2,16 +2,16 @@ rm -rf client/build
 rm -rf client/build-switch
 rm -rf client/cmake-build-debug
 rm -rf client/cmake-build-release
-rm -rf client/out
+rm -rf client/bin
 
 rm -rf common/build
 rm -rf common/build-switch
 rm -rf common/cmake-build-debug
 rm -rf common/cmake-build-release
-rm -rf common/out
+rm -rf common/bin
 
 rm -rf server/build
 rm -rf server/build-switch
 rm -rf server/cmake-build-debug
 rm -rf server/cmake-build-release
-rm -rf server/out
+rm -rf server/bin
